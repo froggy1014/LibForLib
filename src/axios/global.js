7efc06b8058e8e0c3486af7fce5fa648a49f9c11
froggy <clcl6084@gmail.com>
2022-09-18 01:31:50 +0900

    Axios Custom Instance Created

--- src/axios/global.js
+++ src/axios/global.js
@@ -1,6 +1,3 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.example.com';
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-axios.defaults.headers.post['Content-Type'] =
-  'application/x-www-form-urlencoded';
+axios.defaults.headers.common['Accept'] = 'application/json';
