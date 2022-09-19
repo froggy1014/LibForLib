bfdee3d470143114d71995eb41d4af25c2cac5ae
froggy <clcl6084@gmail.com>
2022-09-19 16:43:41 +0900

    SVG Icon code edited

--- chakra-app/public/icons/Icon.tsx
+++ chakra-app/public/icons/Icon.tsx
@@ -1,6 +1,12 @@
-export const ShieldIcon = () => {
+export const ShieldIcon = ({ ...props }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="46"
+      height="46"
+      viewBox="0 0 23 23"
+    >
       {' '}
       <g>
         {' '}
@@ -11,14 +17,13 @@ export const ShieldIcon = () => {
   );
 };
 
-export const ClipIcon = () => {
+export const ClipIcon = ({ ...props }) => {
   <svg
+    {...props}
     xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
+    viewBox="0 0 48 48"
     fill="currentColor"
     className="bi bi-bookmark-star"
-    viewBox="0 0 16 16"
   >
     {' '}
     <path d="M7.84 4.1a.178.178 0 0 1 .32 0l.634 1.285a.178.178 0 0 0 .134.098l1.42.206c.145.021.204.2.098.303L9.42 6.993a.178.178 0 0 0-.051.158l.242 1.414a.178.178 0 0 1-.258.187l-1.27-.668a.178.178 0 0 0-.165 0l-1.27.668a.178.178 0 0 1-.257-.187l.242-1.414a.178.178 0 0 0-.05-.158l-1.03-1.001a.178.178 0 0 1 .098-.303l1.42-.206a.178.178 0 0 0 .134-.098L7.84 4.1z" />{' '}
@@ -26,8 +31,8 @@ export const ClipIcon = () => {
   </svg>;
 };
 
-export const RewindIcon = () => {
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+export const RewindIcon = ({ ...props }) => {
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23">
     {' '}
     <g>
       {' '}
